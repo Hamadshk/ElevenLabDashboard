aff98f958eb351948afcab9c880abac00d675094
Ashar Nadeem <ashar.nadeem@kpme.com>
2025-07-23 18:43:03 +0500

    redirection and readme updated

---
+++ el_labs_dashboard/pages/index.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/dashboard");
+  }, [router]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "18px",
+        color: "#003366",
+      }}
+    >
+      Redirecting to dashboard...
+    </div>
+  );
+}
